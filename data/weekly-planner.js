aa9c5e575991611436530568e515ffd0123d6161
DoribelTerceroParker <dtercerocunystart@gmail.com>
2024-04-13 11:42:16 -0400

    filling weekly-planner data

---
+++ data/weekly-planner.js
@@ -0,0 +1,7 @@
+const weeklyPlanner = [
+  {day: "Monday", item: "Clean", urgency: "medium-priority", completed: false},
+  {day: "Tuesday", item: "Study", urgency: "low-priority", completed: true},
+  {day: "Wednesday", item: "Workout", urgency: "high-priority", completed: false},
+  {day: "Thursday", item: "Meditate", urgency: "medium-priority", completed: false},
+  {day: "Friday", item: "Cook", urgency: "medium-priority", completed: false},
+]
